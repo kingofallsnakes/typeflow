cdba22f766390ae0ba90c42986b7304dd861ba52
kingofallsnakes <srivan2318@gmail.com>
2026-09-12 12:05:27 +0530

    Remove Netlify configuration, dependency, and build plugin for manual upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
-import netlify from "@netlify/vite-plugin-tanstack-start";
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tailwindcss(),
     tanstackStart({
       server: { entry: "server" },
     }),
-    command === "build" ? netlify() : null,
     react(),
-  ].filter(Boolean),
-}));
+  ],
+});
+
